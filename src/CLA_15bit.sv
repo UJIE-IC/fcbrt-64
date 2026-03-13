3725e6a382c40f73a68d800ccc9eecb683474228
yaoyujie <yaoyujie1228@126.com>
2026-03-13 15:53:07 +0800

    MLPLAC

---
+++ src/CLA_15bit.sv
@@ -0,0 +1,24 @@
+module CLA_15bit (
+    input  logic [14:0] A,
+    input  logic [14:0] B,
+    input  logic        C_in,
+    output logic [14:0] S,
+    output logic        C_out
+);
+    logic [14:0] P = A ^ B; 
+    logic [14:0] G = A & B; 
+    logic [15:0] C; 
+
+    assign C[0] = C_in;
+
+    genvar i;
+    generate
+        for (i = 0; i < 15; i = i + 1) begin : cla_logic
+            assign C[i+1] = G[i] | (P[i] & C[i]);
+            assign S[i]   = P[i] ^ C[i];
+        end
+    endgenerate
+
+    assign C_out = C[15];
+    
+endmodule
